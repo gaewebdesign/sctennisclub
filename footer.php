a16c00e724cd21bee9545b8f45829b4d3e1cb9a4
githubmutt@gmail.com <github.mutt@gmail.com>
2023-08-23 22:34:19 -0700

    footer for all html

---
+++ footer.php
@@ -0,0 +1,13 @@
+<div class="row">
+                  <div class="col-sm-12"> &nbsp;</div>                   
+                  <div class="col-sm-12"> &nbsp;
+
+                     <a href="board.html" >About Us</a> | 
+                     <a href="membership.html">Membership</a>  | 
+                     <a href="mailto: info@sctennisclub.org" >Contact Us</a> |
+                     <a href="https://www.facebook.com/santaclaratennisclub" ><img src="./images/fb.png"></a> | 
+
+                     &copy;2024 Santa Clara Tennis Club </span>
+                  </div> 
+                  <div class="col-sm-12"> &nbsp;</div> 
+            </div>
