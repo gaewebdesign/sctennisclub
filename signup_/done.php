11fc7a6c5ee690df89834312f956735d31774291
githubmutt@gmail.com <github.mutt@gmail.com>
2023-09-19 01:55:42 -0700

    cancel,done

--- signup_/done.php
+++ signup_/done.php
@@ -4,9 +4,61 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/index.css" >
+
     <title>Document</title>
+   
+
 </head>
-<body>
-    <h1> DONE </h1>
+<body class="Back">
+    <center>x
+    <h1> Thanks <?php echo( $_GET["item_number"]);?> for signing up to dinner !
+    <p> View your name at <br>
+    <a href="http://www.sctennisclub.org/signup">www.sctennisclub.org/signup</a>
+    </h1>
+    <img  class="RESP_IMAGE" src="./images/Athenat.png">
+
+    <h1>
+    <a href="https://www.google.com/maps/place/Athena+Grill+%26+Catering/@37.3764667,-121.9590693,17z/data=!3m1!4b1!4m6!3m5!1s0x808fca283052b81f:0x97b199e3951a5818!8m2!3d37.3764667!4d-121.9564944!16s%2Fg%2F1hbpwrv7r?entry=ttu"
+    >
+    
+    1505 Space Park Dr, Santa Clara 
+
+    </a></h1>
+
+    <h1>Follow the action on the club's Instagram page! </h1>
+
+    <a href="https://www.instagram.com/santaclaratennisclub/"> 
+
+        <img class="RESP_IMAGE" src = "./images/instagram.png">
+
+    </a>
+
+    <?php
+  /*
+   http://www.sctennisclub.net/signup_/done?PayerID=DA9ASGTZSCJE4&st=Completed&tx=66V397497W021782U&cc=USD&amt=0.10&cm=1693708024&payer_email=tennis.mutt%40gmail.com&payer_id=DA9ASGTZSCJE4&payer_status=VERIFIED&first_name=Roger&last_name=Okamoto&txn_id=66V397497W021782U&mc_currency=USD&mc_fee=0.10&mc_gross=0.10&protection_eligibility=ELIGIBLE&payment_fee=0.10&payment_gross=0.10&payment_status=Completed&payment_type=instant&handling_amount=0.00&shipping=0.00&item_name=SCTC%20Dinner%3A%20Vegetarium%20Mousaka&item_number=%20Vegan%20Vegas%20&quantity=1&txn_type=web_accept&payment_date=2023-09-03T09%3A27%3A05Z&receiver_id=TV6SA58WGBDYA&notify_version=UNVERSIONED&custom=1693708024&verify_sign=A19W9IvoUO9.MI.dOSNsUSXL9nIcAZtOzM29KxMYCSc.O9TiFsPuyIJJ
+
+  */
+
+      include "../library/include.inc";
+
+      $NAME = $_GET["item_number"];
+      $EMAIL = $_GET["payer_email"];
+      $CUSTOM = $_GET["custom"];
+ 
+
+       // copy over from the mixer_pending to the mixer directory
+       LOGGER("done.php: values $NAME $EMAIL $CUSTOM ") ;
+       LOGGER("done.php: copyto".TABLE_MIXER_PENDING." to  ".TABLE_MIXER." $CUSTOM  ") ;
+
+//       TEXT("Sept log: $NAME $EMAIL $CUSTOM ") ;
+//       TEXT("TABLES: ".TABLE_MIXER_PENDING." to  ".TABLE_MIXER." $CUSTOM  ") ;
+
+//       copyto( TABLE_MIXER_PENDING,  TABLE_MIXER, $CUSTOM);
+
+     ?>
+
+
+
 </body>
 </html>
