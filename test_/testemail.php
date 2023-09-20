2ac8ff4430a0f10e27a9d4a5d7071cdb9e67fbb2
githubmutt@gmail.com <github.mutt@gmail.com>
2023-09-20 02:58:47 -0700

    email

--- test_/testemail.php
+++ test_/testemail.php
@@ -4,10 +4,26 @@ include "../library/include.inc";
 
 TEXT("test email");
 
-$subject = "test email";
-$message = "message";
-EMAILER($subject, $message, $verbose=false);
+$to = "tennis.mutt@gmail.com";
+$subject = "subject test email";
+$message = "mymessage";
 
-TEXT("message sent");
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$headers .= "From: membership@sctennisclub.org  \r\n";
+
+echo($to);
+echo("<br>");
+
+echo($subject);
+echo("<br>");
+
+echo($headers);
+echo("<br>");
+
+$r=mail($to,$subject,$message,$headers);
+
+echo($r);
+
 
 ?>
