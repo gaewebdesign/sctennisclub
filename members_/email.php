5a36f92c09f7045a1866b65b7426487378d0b8af
githubmutt@gmail.com <github.mutt@gmail.com>
2023-09-27 04:56:29 -0700

    tolower

--- members_/email.php
+++ members_/email.php
@@ -130,7 +130,7 @@ Please Note: Starting May 1st, our phone availability will change from 24/7 to M
                     echo("</td>");
 
                     echo("<td>");
-                    echo($row['email']);
+                    echo( strtolower($row['email']) );
                     echo("</td>");
 
                     echo("</tr> ");
