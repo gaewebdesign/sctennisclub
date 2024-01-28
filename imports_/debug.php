f92d78ff025d084b2ca55a5730e66db4a7c386fd
githubmutt@gmail.com <github.mutt@gmail.com>
2024-01-27 18:32:40 -0800

    debug.php

--- imports_/debug.php
+++ imports_/debug.php
@@ -1,9 +1,9 @@
 <a href="./index.phtml"> Home </a> |   
 
-       <a href="./join_debug.html">Join </a> |
-       <a href="./members_debug.html" >Members </a>|
-       <a href="./pending.html">Pending</a>|
-       <a href="./logger.html">Logger</a>|
+       <a href="./join_debug.phtml">Join </a> |
+       <a href="./members_debug.phtml" >Members </a>|
+       <a href="./pending.phtml">Pending</a>|
+       <a href="./logger.phtml">Logger</a>|
 
        <a href="./signup_debug.html" >Signup </a>|
        <a href="./pending_mixer.html" >Pending </a>|
