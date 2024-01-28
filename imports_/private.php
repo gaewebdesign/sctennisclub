23323d15b0cf75e07ce23b3f9dd12f50cb2a5f1d
githubmutt@gmail.com <github.mutt@gmail.com>
2024-01-27 18:16:20 -0800

    email_join

--- imports_/private.php
+++ imports_/private.php
@@ -1,8 +1,8 @@
 <a href="./index.phtml"> Home </a> |   
 
 
-<a href="./email.html" >Email </a> |
-<a href="./email_join.html" >Athena Dinner</a>
+<a href="./email.phtml" >Email </a> |
+<a href="./email_join.phtml" >Athena Dinner</a>
 
 </a
              
