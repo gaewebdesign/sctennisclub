c3dba9872f6dfa8c295c1be5a318ba6c1ab984e2
githubmutt@gmail.com <github.mutt@gmail.com>
2024-02-04 23:57:59 -0800

    footer

--- imports_/footer.php
+++ imports_/footer.php
@@ -2,8 +2,8 @@
                   <div class="col-sm-12"> &nbsp;</div>                   
                   <div class="col-sm-12"> &nbsp;
 
-                     <a href="board.phtml" >About Us</a> | 
-                     <a href="membership.phtml">Membership</a>  | 
+                     <a href="./board" >About Us</a> | 
+                     <a href="./membership">Membership</a>  | 
                      <a href="mailto: info@sctennisclub.org" >Contact Us</a> |
                      <a href="https://www.facebook.com/santaclaratennisclub" ><img src="./images/fb.png"></a> | 
                      <a href="https://www.instagram.com/santaclaratennisclub" ><img src="./images/instagram.png"></a> | 
