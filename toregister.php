da121793f8864c60bd7931f6244650cdd4428fff
githubmutt@gmail.com <github.mutt@gmail.com>
2024-02-19 20:26:56 -0800

    toregister

--- toregister.php
+++ toregister.php
@@ -7,8 +7,35 @@ include "./library/emailer.php";
 $fname = $_POST['fname'];
 $lname = $_POST['lname'];
 $email = $_POST['email'];
-$password = $_POST['password'];
+$team = $_POST["team"];
+$ntrp = $_POST["ntrp"];
+
+$daytime="";
+if (  isset($_POST["daytime"])  )
+     $daytime = "y";
+
+// Resident Captain
+$rescaptain="NR";
+if (  isset($_POST["rescaptain"]) ){
+    LOGGER("$fname $lname is resident");
+    $rescaptain="Res";
+}else{
+    LOGGER("$fname $lname is NOT resident");
+}
+
+// SCTC previous membership
+$resprev="";
+if (  isset($_POST["resprev"]) ) $resprev="y";
+
+// Resident Count
+$rescount = $_POST["rescount"];
+
+
+//$password = $_POST['password'];
 $password ="4pigs";
+
+
+
 
 $error=false;
 if($password=="4pigs"){
@@ -34,6 +61,7 @@ if( $error == true) return;
 $custom = time()-60*60*7;
 $dt = new DateTime("@$custom");
 $date = ltrim($dt->format('m/d/Y'),0);
+$date = $custom;
 
 $theTABLE = TABLE_REGISTER;
 
@@ -41,19 +69,23 @@ $theTABLE = TABLE_REGISTER;
 //toDB($theTABLE, $fname,$lname,$gender,$ntrp,$email, $member,$paid,$date,$custom,$event);
 //$fname = "Stacia";
 //$lname = "Simmons";
-$team = "Mx";
-$ntrp = "8.0";
-$daytime = "n";
-$email = "s.simmons@gmail.com";
-$rescaptain = "y";
-$resprev="y";
-$rescount=2;
-$year=2024;
-$date=1705711820;
+//$team = "Mx";
+//$ntrp = "8.0";
+
+//$daytime="n";
+//if($_POST["daytime"] == "y") $daytime="y";
+//$lname .= $email;
+//$email = "s.simmons@gmail.com";
+//$rescaptain = "y";
+//$resprev="y";
+//$rescount=2;
+$year=YEAR;
+//$date=1705711820;
 $insignia="";
 $custom="";
 $pwd="4pigs";
 
+// change
 toRegisterDB($theTABLE, $fname,$lname,$team, $ntrp , $daytime , $email,$rescaptain,$resprev , $rescount, $year , $date, $insignia, $custom, $pwd);
 
 sendemail($fname." ".$lname, $email, "SCTENNISCLUB.NET TEST => $theTABLE ");
