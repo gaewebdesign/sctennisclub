850ad3288c559ddff0e03b4c2eacde501a2c5c83
githubmutt@gmail.com <github.mutt@gmail.com>
2024-02-21 23:16:53 -0800

    test.php

--- library/email/test.php
+++ library/email/test.php
@@ -5,6 +5,44 @@ include "email.inc";
 // calling from email.inc"
 $subject="testing phpemailer";
 $message = "test message";
+$recipient = "R";
+$vac_title = "title";
+$vac_id = "vac_id";
+
+$message = "
+<html>
+<head>
+<title>Rate Your Experience</title>
+</head>
+<body bgcolor = '#0000FF'>
+<center>
+
+<p>
+    <h2>How well did '$recipient' do?</h2>
+    <h4>Vacancy Title: '$vac_title'</h4>
+    <h4>Vacancy ID: '$vac_id'</h4>
+</P>
+</center>
+<center>
+    <table>
+    <tr>
+    <td><h3>we are built on sharing your review with other people who may also want to use this service.</h3></td>
+    </tr>
+    <tr>
+    <td><h3>Please take a minute to reflect on the level of service youve received.</h3></td>
+    </tr>
+    <tr>
+    <td><h3>Your review is public - to other employers registered with Ahoy Employ.</h3></td>
+    </tr>
+    <tr>
+    <td><h3>Should you have any concerns that you would prefer to share in private, please email us at info@website.ca and quote the Vacancy ID shown above.</h3></td>
+    </tr>
+    </table>
+</center>
+</body>
+</html>
+";
+
 
 phpemailer($subject,$message);
 
