34a5c2bead66d2d322d964efcce7d2575c91b02b
githubmutt@gmail.com <github.mutt@gmail.com>
2024-04-13 23:22:13 -0700

    racquet store

--- imports_/navlink.php
+++ imports_/navlink.php
@@ -8,6 +8,10 @@
                   <a href="./board" >Board</a>  |
                   <a href="./usta" >USTA  
 <!--                  <a href="./register" class="Link_Register">Register </a> |   -->
-                  ||
+                  |
                   <a href="https://www.ustanorcal.com/organization.asp?id=20">SCTC Teams </a>
-             
+                  || &nbsp;
+                  
+                  <a href="http://www.racquetstore.com"> 
+                  <img src="./images/racquet_store_logo.png" width="25px" > Racquet Store </a>
+                
