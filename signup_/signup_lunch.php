532f18215aed410eebbbc00d1b1edf968e59ec79
githubmutt@gmail.com <github.mutt@gmail.com>
2024-04-21 22:01:14 -0700

    signup

--- signup_/signup_lunch.php
+++ signup_/signup_lunch.php
@@ -228,7 +228,7 @@
           <button class="w-100 btn btn-primary btn-lg" disabled >Goto Paypal</button>
 <button class="w-100 btn btn-primary btn-lg" disabled > Signup is Over! </button>
 -->
-<button class="w-100 btn btn-primary btn-lg" enabled > Signup is Over! </button>
+<button class="w-100 btn btn-primary btn-lg" disabled > Signup is Over! </button>
 
         </form>
       </div>
