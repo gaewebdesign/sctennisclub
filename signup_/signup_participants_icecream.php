bccfa3ee681b90c2dbca8198b9c2092ebafba376
githubmutt@gmail.com <github.mutt@gmail.com>
2024-06-27 15:52:53 -0700

    DATE

--- signup_/signup_participants_icecream.php
+++ signup_/signup_participants_icecream.php
@@ -40,7 +40,7 @@ tr {
           $query = "select * from ".TABLE_MIXER_PAYPAL."  where custom>$jurassic and custom<$cretaceous order by fname asc";
           $query = "select * from ".TABLE_MIXER_PENDING."  where date>$jurassic and date<$cretaceous order by fname asc";
 
-          $query = "select * from ".TABLE_MIXER_PENDING." where date>$jurassic" ;
+          $query = "select * from ".TABLE_MIXER_PENDING." where date>$jurassic  order by lname";
 
           echo( "QUERY");
           echo $query;
