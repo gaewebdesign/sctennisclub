4db3ec8c5538d3a2d00646101fdbf426a1052810
githubmutt@gmail.com <github.mutt@gmail.com>
2024-06-27 23:56:55 -0700

    ic

--- signup_/signup_icecream.php
+++ signup_/signup_icecream.php
@@ -62,7 +62,7 @@
       </div>
       <div class="col-md-7 col-lg-8">
         <h4 class="mb-3">Info (---one person per entry)</h4>
-        <form class="needs-validation" novalidate name="signup" action="topaypal_mixer_captcha_icecrea.php", method="post" >
+        <form class="needs-validation" novalidate name="signup" action="topaypal_mixer_captcha_icecream.php", method="post" >
           <div class="row g-3">
             <div class="col-sm-6">
               <label for="firstName" class="form-label">First name</label>
