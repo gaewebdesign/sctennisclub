06c2247ddc6b612e83055079d481203345c44ede
githubmutt@gmail.com <github.mutt@gmail.com>
2024-06-27 21:09:37 -0700

    navlink

--- imports_/navlink.php
+++ imports_/navlink.php
@@ -3,9 +3,7 @@
                   <a href="./membership" >Membership </a>|
                   <a href="./join" class="Link_Red" >Join </a>|                 
                   <a href="./members" >Members </a>|
-<!--                  <a href="./signup" class="Link_Purple">June Signup</a>| 
--->
-
+                  <a href="./signup" class="Link_Purple">Pigout Signup</a>| 
                   <a href="./mixers" >Mixers </a> |
                   <a href="./june" class="Link_Purple">June</a>|
                   <a href="./board" >Board</a>  |
@@ -15,6 +13,8 @@
                   <a href="https://www.ustanorcal.com/organization.asp?id=20">SCTC Teams </a>
                   || &nbsp;
                   
+
+                  // nav
                   <a href="http://www.racquetstore.com"> 
                   <img src="./images/racquet_store_logo.png" width="25px" > Racquet Store </a>
                 
