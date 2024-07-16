e7fee5ffb1e82e34084172936fa852251d8cfed3
githubmutt@gmail.com <github.mutt@gmail.com>
2024-07-16 16:42:25 -0700

    tourny

---
+++ tournament_/mens.php
@@ -0,0 +1,114 @@
+<html>
+
+   <h1>2024 Mens Singles Tournament</h1>
+
+   <div class="container">
+  <div class="row">
+    <div class="col-sm">
+      Quarter Finals<br>
+      <div class="small">Finish by 8/1/2024</div>
+    </div>
+    <div class="col-sm">
+      Semi Finals
+      <div class="small">Finish by 8/15/2024</div>
+    </div>
+    <div class="col-sm">
+      Finals
+      <div class="small">Finish by 8/22/2024</div>
+    </div>
+  </div>
+</div>
+
+
+<body>
+<main id="tournament2">
+<?php
+//$q = array( "Carlos Alcarez", "Tommy Paul", "Daniell Medvedev","Jannik Sinner");
+
+$q[] = "Carlos Alcarez";
+$q[] = "Tommy Paul";
+$q[] = "Danill Medvedev";
+$q[]= "Jannik Sinner";
+$q[] = "Lorenzo Mussetti";
+$q[] = "Taylor Fritz";
+$q[] = "Alex de Minaur";
+$q[] = "Novak Djokovic";
+
+
+$q1 = "Carlos Alcarez";
+$q2 = "Tommy Paul";
+$q3 = "Danill Medvedev";
+$q4 = "Jannik Sinner";
+
+$q5 = "Lorenzo Mussetti";
+$q6 = "Taylor Fritz";
+$q7 = "Alex de Minaur";
+$q8 = "Novak Djokovic";
+
+?>
+<ul class="round">
+  <li class="spacer">
+  <li class="game-top"> <?php echo $q[0] ?> <span></span> </li>
+  <li class="draw"></li>
+  <li class="game-top"> <?php echo $q[1] ?> <span></span> </li>
+  <li class="spacer">  
+
+<!--   -->
+  <li class="game-top"> <?php echo $q[2] ?> <span></span> </li>
+  <li class="draw"></li>
+  <li class="game-top"><?php echo $q[3] ?><span></span> </li>
+
+  <li class="spacer"> 
+<!--   -->
+
+<li class="game-top"><?php echo $q[4] ?> <span></span> </li>
+  <li class="draw"></li>
+  <li class="game-top"><?php echo $q[5] ?> <span></span> </li>
+
+  <li class="spacer"> 
+<!--   -->
+<li class="game-top"> <?php echo $q[6] ?> <span></span> </li>
+  <li class="draw"></li>
+  <li class="game-top"><?php echo $q[7] ?>  <span></span> </li>
+
+  <li class="spacer"> 
+<!--   -->
+
+
+
+</ul>
+
+<!-- Semi-Finals  -->
+<ul class="round">
+<li class="game-top"> Alcaraz <span></span> </li>
+  <li class="draw draw2"></li>
+  <li class="game-top"> Medvedev <span></span> </li>
+  <li class="spacer"> 
+  <li class="spacer"> 
+<li class="game-top">L Musetti <span></span> </li>
+  <li class="draw draw2"></li>
+  <li class="game-top"> Djokovich <span></span> </li>
+</ul>
+
+ <!-- Finals -->
+  <ul class="round">
+  <li class="game-top"> Alcaraz <span></span> </li>
+  <li class="draw draw3"></li>
+  <li class="game-top"> Djokovich <span></span> </li>
+
+</ul>
+
+ <!-- Winner -->
+<ul class="round">
+
+<li class="game-top"> Alcaraz <span></span> </li>
+</ul>
+
+</main>
+
+
+
+<body>
+      
+</body>
+</html>
---
+++ tournament_/womens.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body >
+<p>
+
+</p>
+
+<h1>Womens Singles Tournament</h1>
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      Quarter Finals
+    </div>
+    <div class="col-sm">
+      Semi Finals
+    </div>
+    <div class="col-sm">
+      Finals
+    </div>
+  </div>
+</div>
+<main>
+
+<ul class="round">
+  <li class="spacer">
+  <li class="game-top"> Barbora Krejikova <span></span> </li>
+  <li class="draw"></li>
+  <li class="game-top"> Jelena Ostapenko <span></span> </li>
+
+  <li class="spacer">  
+
+<!--   -->
+  <li class="game-top"> Elena Rybakna <span></span> </li>
+  <li class="draw"></li>
+  <li class="game-top"> Elina Svitolina <span></span> </li>
+
+  <li class="spacer"> 
+<!--   -->
+
+<li class="game-top">Donna Vekic <span></span> </li>
+  <li class="draw"></li>
+  <li class="game-top"> Lulu Sun <span></span> </li>
+
+  <li class="spacer"> 
+<!--   -->
+<li class="game-top"> Jasmine Paolini <span></span> </li>
+  <li class="draw"></li>
+  <li class="game-top"> Emma Navarro  <span></span> </li>
+
+  <li class="spacer"> 
+<!--   -->
+
+
+
+</ul>
+<!-- Semi-Finals  -->
+<ul class="round">
+<li class="game-top"> Krejikova<span></span> </li>
+  <li class="draw draw2"></li>
+  <li class="game-top"> Rybakna <span></span> </li>
+  <li class="spacer"> 
+  <li class="spacer"> 
+<li class="game-top">Vekic <span></span> </li>
+  <li class="draw draw2"></li>
+  <li class="game-top"> Paolini <span></span> </li>
+</ul>
+
+ <!-- Finals -->
+  <ul class="round">
+  <li class="game-top"> Krejikova <span></span> </li>
+  <li class="draw draw3"></li>
+  <li class="game-top"> Paolini <span></span> </li>
+
+</ul>
+
+ <!-- Winner -->
+<ul class="round">
+
+<li class="game-top"> Barbora Krejcikova <span></span> </li>
+</ul>
+
+</main>
+
+
+</html>
