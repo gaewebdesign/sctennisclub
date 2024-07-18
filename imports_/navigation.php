c242328e1e9550dd6f64770047991f60bf597558
githubmutt@gmail.com <github.mutt@gmail.com>
2024-07-18 03:48:36 -0700

    nav

--- imports_/navigation.php
+++ imports_/navigation.php
@@ -5,20 +5,55 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
+<style>
+ .navbar-brand{
+   font-size: smaller;
+ }
+
+ .dropdown-item{
+
+#
+#    background-color: #fff; /* here */
+#    background: #fff;
+#    background-color: rgb(202,233,245);
+    
+ }
+
+</style>
 
 </head>
+<!--
+
+https://getbootstrap.com/docs/5.1/getting-started/introduction/
+
+-->
+<!--
+<a href="./index.phtml"> Home </a> |   
+                  <a href="./info"> Info </a> |          
+                  <a href="./membership" >Membership </a>|
+                  <a href="./join" class="Link_Red" >Join </a>|                 
+                  <a href="./members" >Members </a>|
+                  <a href="./signup" class="Link_Purple">Pigout Signup</a>| 
+                  <a href="./mixers" >Mixers </a> |
+                  <a href="./june" class="Link_Purple">June</a>|
+                  <a href="./board" >Board</a>  |
+                  <a href="./usta" >USTA  
+-->
+
 
 
 <nav class="navbar navbar-expand-lg">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Home</a> 
-      <a class="navbar-brand" href="#">Info</a>
-      <a class="navbar-brand" href="#">Membership</a>
-      <a class="navbar-brand" href="#">Join</a>
-      <a class="navbar-brand" href="#">Members</a>
-      <a class="navbar-brand" href="#">Pigout Signup</a>
-      <a class="navbar-brand" href="#">Mixers</a>
-      <a class="navbar-brand" href="#">USTA</a>
+      <a class="navbar-brand" href="./index.phtml">Home</a> 
+      <a class="navbar-brand" href="./info">Info</a>
+      <a class="navbar-brand" href="./membership">Membership</a>
+      <a class="navbar-brand" href="./join">Join</a>
+      <a class="navbar-brand" href="./members">Members</a>
+      <a class="navbar-brand" href="./signup">Pigout Signup</a>
+      <a class="navbar-brand" href="./mixers">Mixers</a>
+      <a class="navbar-brand" href="./june" class="Link_Purple">June</a>
+      <a class="navbar-brand" href="./board">Board</a>
+      <a class="navbar-brand" href="./usta">USTA</a>
 
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -31,10 +66,10 @@
             </a>
             <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
               <li><a class="dropdown-item" href="#">Info</a></li>
-              <li><a class="dropdown-item" href="#">Signup</a></li>
+              <li><a class="dropdown-item disabled" href="#">Signup</a></li>
               <li><a class="dropdown-item" href="#">Players</a></li>
-              <li><a class="dropdown-item" href="#">Draws (Mens Singles)</a></li>
-              <li><a class="dropdown-item" href="#">Draws (Womens Singles)</a></li>
+              <li><a class="dropdown-item" href="./tournament">Draws (Mens Singles)</a></li>
+              <li><a class="dropdown-item" href="./tournament">Draws (Womens Singles)</a></li>
 
 
 
