07955cc736415774b39de5897319574e416d691f
githubmutt@gmail.com <github.mutt@gmail.com>
2024-07-18 02:55:07 -0700

    hier menu

---
+++ imports_/navigation.php
@@ -0,0 +1,51 @@
+<html>
+
+<head>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+</head>
+
+
+<nav class="navbar navbar-expand-lg">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Home</a> 
+      <a class="navbar-brand" href="#">Info</a>
+      <a class="navbar-brand" href="#">Membership</a>
+      <a class="navbar-brand" href="#">Join</a>
+      <a class="navbar-brand" href="#">Members</a>
+      <a class="navbar-brand" href="#">Pigout Signup</a>
+      <a class="navbar-brand" href="#">Mixers</a>
+      <a class="navbar-brand" href="#">USTA</a>
+
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Tournament
+            </a>
+            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+              <li><a class="dropdown-item" href="#">Info</a></li>
+              <li><a class="dropdown-item" href="#">Signup</a></li>
+              <li><a class="dropdown-item" href="#">Players</a></li>
+              <li><a class="dropdown-item" href="#">Draws (Mens Singles)</a></li>
+              <li><a class="dropdown-item" href="#">Draws (Womens Singles)</a></li>
+
+
+
+
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+ </p>
+      
+</body>
+</html>
