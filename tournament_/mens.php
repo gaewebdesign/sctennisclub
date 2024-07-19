d69534973daa68a9dfec49ae422c49ba5f1ff8a0
githubmutt@gmail.com <github.mutt@gmail.com>
2024-07-18 20:03:31 -0700

    tourny

--- tournament_/mens.php
+++ tournament_/mens.php
@@ -6,14 +6,14 @@
 
  <ul class="round">
    Quarter-Finals
-   <div class="small">Finish by 8/4/2024</div>
+   <div class="small">Finish by 9/7/2024</div>
   </ul>
   <ul class="round">
    Semi-Finals
-   <div class="small">Finish by 8/18/2024</div></ul>
+   <div class="small">Finish by 9/21/2024</div></ul>
   <ul class="round">
    Finals
-   <div class="small">Finish by 9/1/2024</div>
+   <div class="small">Finish by 10/4/2024</div>
   </ul>
   
 </main>
--- tournament_/womens.php
+++ tournament_/womens.php
@@ -12,14 +12,14 @@
 
 <ul class="round">
   Quarter-Finals
-  <div class="small">Finish by 8/4/2024</div>
+  <div class="small">Finish by 8/31/2024</div>
  </ul>
  <ul class="round">
   Semi-Finals
-  <div class="small">Finish by 8/18/2024</div></ul>
+  <div class="small">Finish by 8/14/2024</div></ul>
  <ul class="round">
   Finals
-  <div class="small">Finish by 9/1/2024</div>
+  <div class="small">Finish by 8/28/2024</div>
  </ul>
  
 </main>
