dbae3390a0029557f47fa62310b513120fedb7d1
githubmutt@gmail.com <github.mutt@gmail.com>
2024-07-18 21:35:17 -0700

    participants

---
+++ tournament_/tournament_participants.php
@@ -0,0 +1,60 @@
+<style>
+tr {
+    line-height: 14px;
+    min-height: 14px;
+    height: 4px;
+    color: blue;
+ }
+ </style>
+
+<p>
+<table class="table table-bordered table-striped table-condensed">
+
+      <thead>
+        <tr>
+        <th style="width:20%">Name</th>
+        <th style="width:10%">NTRP</th>
+        <th style="width:20%">Name</th>
+        <th style="width:10%">NTRP</th>
+
+        </tr>
+      </thead>
+      <tbody>
+           <p>
+           <h4>Mx 7.5 </h4> 
+           <p>
+           <tr><td> W Chen <td>M4.0  <td>  P Lian  <td>W3.0 </tr>
+           <tr><td> L Ke <td>M4.0  <td>  X Meir  <td>W3.0 </tr>
+           <tr><td> D Yen <td>M4.0  <td>  Q Lin  <td>W3.0 </tr>
+           <tr><td> T Zheng <td>M3.5  <td>  CJ Liang  <td>W4.0 </tr>
+
+
+
+      </tbody>
+    </table>
+
+    <table class="table table-bordered table-striped table-condensed">
+
+<thead>
+  <tr>
+  <th style="width:20%">Name</th>
+  <th style="width:10%">NTRP</th>
+  <th style="width:20%">Name</th>
+  <th style="width:10%">NTRP</th>
+
+  </tr>
+</thead>
+<tbody>
+     <p>
+     <h4>Mx 6.5 </h4> 
+     <p>
+     <tr><td> N Yea <td>W3.0  <td>  J White  <td>M3.5 </tr>
+     <tr><td> S Qui <td>W3.0  <td>  H Seaver  <td>M3.5 </tr>
+     <tr><td> L Yuhan<td>W3.0  <td>  TJ  Xhong  <td>M3.0 </tr>
+
+
+</tbody>
+</table>
+
+
+  </div>
