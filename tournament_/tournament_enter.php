57c2a8456e4099fa32efd45a0d79ab35910c2b0c
githubmutt@gmail.com <github.mutt@gmail.com>
2024-07-19 18:10:32 -0700

    tourny enter

--- tournament_/tournament_enter.php
+++ tournament_/tournament_enter.php
@@ -1,4 +1,154 @@
 
+
+
+<form class="row g-3">
+
+
+
+
+
+<div class="col-md-12">
+    <label for="validationDefault04" class="form-label">Division</label>
+    <select class="form-select" id="validationDefault04" required>
+    <option selected value="">Mx 6.5</option>
+    <option  value="">Mx 7.5</option>
+    </select>
+  </div>
+
+  
+
+  <div class="col-sm-3">
+    <label for="validationDefault01" class="form-label">First name</label>
+    <input type="text" class="form-control fs-6" id="validationDefault01" value="" required>
+  </div>
+  <div class="col-sm-3">
+    <label for="validationDefault02" class="form-label">Last name</label>
+    <input type="text" class="form-control fs-6" id="validationDefault02" value="" required>
+  </div>
+  <div class="col-sm-3">
+    <label for="validationDefaultUsername" class="form-label">Email</label>
+    <div class="input-group">
+<!--      <span class="input-group-text" id="inputGroupPrepend2"></span> -->
+      <input type="text" class="form-control fs-6" id="validationDefaultUsername"  aria-describedby="inputGroupPrepend2" required>
+    </div>
+  </div>
+
+  <div class="col-sm-1">
+    <br/>
+   <div class="input-group"> <p>
+    <label for="validationDefault04" class="form-label">Gender</label> 
+    <select class="form-select" id="validationDefault04" required>
+    <option selected value="M">M</option>
+    <option  value="F">F</option>    
+    
+    </select>
+    </div>
+   </div>
+
+   <div class="col-sm-1">
+    <br/>
+   <div class="input-group">
+    <label for="validationDefault04" class="form-label">NTRP</label> <p>
+    <select class="form-select" id="validationDefault04" required>
+    <option value="3.0">3.0</option>
+    <option selected value="3.5">3.5</option>    
+    <option value="4.0">4.0</option>
+    </select>
+    </div>
+   </div>
+
+
+
+
+  <hr/>
+  <div class="col-sm-3">
+    <label for="validationDefault01" class="form-label">First name</label>
+    <input type="text" class="form-control" id="validationDefault01" value="" required>
+  </div>
+  <div class="col-sm-3">
+    <label for="validationDefault02" class="form-label">Last name</label>
+    <input type="text" class="form-control" id="validationDefault02" value="" required>
+  </div>
+  <div class="col-sm-3">
+    <label for="validationDefaultUsername" class="form-label">Email</label>
+    <div class="input-group">
+<!-- <span class="input-group-text" id="inputGroupPrepend2"></span> -->
+      <input type="text" class="form-control" id="validationDefaultUsername"  aria-describedby="inputGroupPrepend2" required>
+    </div>
+  </div>
+
+
+  <div class="col-sm-1">
+    <br/>
+   <div class="input-group"> <p>
+    <label for="validationDefault04" class="form-label">Gender</label> 
+    <select class="form-select" id="validationDefault04" required>
+    <option  value="M">M</option>
+    <option selected value="F">F</option>    
+    
+    </select>
+    </div>
+   </div>
+
+   <div class="col-sm-1">
+    <br/>
+   <div class="input-group">
+    <label for="validationDefault04" class="form-label">NTRP</label> <p>
+    <select class="form-select" id="validationDefault04" required>
+    <option value="3.0">3.0</option>
+    <option selected value="3.5">3.5</option>    
+    <option value="4.0">4.0</option>
+    </select>
+    </div>
+   </div>
+
+
+
+  <!--
+  <div class="col-md-6">
+    <label for="validationDefault03" class="form-label">City</label>
+    <input type="text" class="form-control" id="validationDefault03" required>
+  </div>
+-->
+
+
+
+<!--
+  <div class="col-md-3">
+    <label for="validationDefault04" class="form-label">Division</label>
+    <select class="form-select" id="validationDefault04" required>
+    <option selected value="">Mx 6.5</option>
+    <option selected value="">Mx 7.5</option>
+    </select>
+  </div>
+
+
+  <div class="col-md-3">
+    <label for="validationDefault05" class="form-label">Zip</label>
+    <input type="text" class="form-control" id="validationDefault05" required>
+  </div>
+-->
+
+
+  <div class="col-12">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required>
+      <label class="form-check-label" for="invalidCheck2">
+        Agree to terms and conditions
+      </label>
+    </div>
+  </div>
+  <div class="col-12">
+    <button class="btn btn-primary" type="submit">Submit form</button>
+  </div>
+</form>
+
+
+
+
+
+
+
 <?php
 
 
@@ -32,7 +182,7 @@ $ntrp2="3.5";
 $division="Mx7.5";
 $date = time()-60*60*7;
 
-toTournyDB($theTABLE,$fname1,$lname1,$email1,$gender1,$ntrp1,$fname2,$lname2,$email2,$gender2,$ntrp2,$year,$division,$team,$mtype,$date,$insignia,$payment,$custom,$opt,$pwd);
+//toTournyDB($theTABLE,$fname1,$lname1,$email1,$gender1,$ntrp1,$fname2,$lname2,$email2,$gender2,$ntrp2,$year,$division,$team,$mtype,$date,$insignia,$payment,$custom,$opt,$pwd);
 
 function toTournyDB($theTABLE,$fname1,$lname1,$email1,$gender1,$ntrp1,$fname2,$lname2,$email2,$gender2,$ntrp2,$year,$division,$team,$mtype,$date,$insignia,$payment,$custom,$opt,$pwd){
 
