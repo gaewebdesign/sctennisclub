2516d07a9eb1ca17ed8f96d51bc1ad82b4a1f8b3
githubmutt@gmail.com <github.mutt@gmail.com>
2024-07-24 02:22:58 -0700

    test.php

---
+++ signup_/test.php
@@ -0,0 +1,104 @@
+
+<div class="py-5 text-center">
+     <picture>
+<!--
+      <img class="mg-fluid img-thumbnail" src="cinco.png" alt="" >
+    <img class="mg-fluid img-thumbnail" src="./images/icecream_trans.gif" alt="-" >
+-->
+
+<img class="mg-fluid img-thumbnail" src="./images/icecreamsocial.png" alt="-" >
+
+
+
+  </picture>
+      <h2>Santa Clara Tennis Club Ice Cream Social </h2>
+
+      
+    <h3 style="font-size: 1.2em ">
+    Enjoy some  tennis and <b>ice cream</b>  on the courts with fellow members for
+    social mixed doubles round robin play at Santa Clara Tennis Center on
+    August 24, 2024 , 9-12 noon on courts #6-#8. Sign up early before entries close!
+    If you just &#x1F497; ice cream, you may
+    sign up for the ice cream only option! <br/>
+    Signup page is at <a href="http://www.sctennisclub.org/signup">www.sctennisclub.org/signup</a>
+  
+    </h3>
+  </div>
+
+<form class="row g-3" action="./signup_/signup_icecream.php" method="post">
+
+
+  <div class="container-fluid">
+    <div class="row align-items-start">
+    <div class="col-sm-3">
+    <label for="validationDefault01" class="form-label">First name</label>
+    <input type="text" class="form-control fs-6" id="validationDefault01" value="" required name="fname1">
+    </div>
+    <div class="col-sm-3">
+        <label for="validationDefault02" class="form-label">Last name</label>
+        <input type="text" class="form-control fs-6" id="validationDefault02" value="" required name="lname1">
+    </div>
+    <div class="col-sm-4">
+        <label for="validationDefaultUsername" class="form-label">Email Address</label>
+        <div class="input-group">
+        <input type="text" class="form-control fs-6" id="validationDefaultUsername"  aria-describedby="inputGroupPrepend2" name="email1" required>
+    </div>
+  </div>
+  </div> <!-- row align-items-start -->  
+</div> <!-- container-fluid  -->
+
+  <div class="container-fluid">
+
+  <div class="mt-4 col-sm-6">
+              <!--<span><h2 style="color:rgb(25, 0, 255)">Select one: </h2></span>  -->
+
+              <div class="form-check ">
+                <input class="form-check-input"  type="radio" id="member" checked="checked" value=<?php echo($MEMBER_FEE); ?> name="mixer"    >
+                <label class="form-check-label" for="member">🎾 &nbsp;Ice cream and Tennis</label>
+                <br/>
+                <input  class="form-check-input" type="radio" id="icecream" value=<?php echo($ICECREAM_FEE); ?> name="mixer">
+                <label class="form-check-label" for="member">&#x1F49C;&nbsp;Ice Cream only!</label>
+
+              </div>
+            </div>
+
+
+</div>
+
+
+
+
+
+
+
+<div class="container-fluid mt-2">
+   <div class="col-md-4"> </div>
+
+   <div class="col-md-4">
+    <label for="validationDefault05" class="form-label">Enter Keycode</label>
+    <input type="text" class="form-control" id="validationDefault05" name="secretcode" required>
+  </div>
+  
+
+  </div>
+  <hr/>
+    
+
+    <div class="mt-5 col-12">
+    <button class="btn btn-primary" name="Button" type="submit">Go to Paypal</button>
+  </div  >
+</div>
+
+
+</div>
+
+
+
+  <hr/>
+
+
+</form>
+
+
+
+
