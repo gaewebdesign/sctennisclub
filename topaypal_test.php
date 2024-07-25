8b8dcb45d3b8f51ce88646768397c9800fbe3a6a
githubmutt@gmail.com <github.mutt@gmail.com>
2024-07-24 17:15:10 -0700

    ice cream

--- topaypal_test.php
+++ topaypal_test.php
@@ -64,8 +64,8 @@ $date = ltrim($dt->format('m/d/Y'),0);
 $theTABLE = "mixer_pending";
 
 echo ("INSERT $fname $lname $paid $date $custom");
-toDB($theTABLE, $fname,$lname,$gender,$ntrp,$email, $member,$paid,$date,$custom,$event);
-sendemail($fname." ".$lname, $email, "SCTENNISCLUB.NET TEST => $theTABLE ");
+//toDB($theTABLE, $fname,$lname,$gender,$ntrp,$email, $member,$paid,$date,$custom,$event);
+//sendemail($fname." ".$lname, $email, "SCTENNISCLUB.NET TEST => $theTABLE ");
 
 $paypal->output_form();
 
