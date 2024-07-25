4c67db7be850cf9f7e8e72d3ed354deb21fbe4ee
githubmutt@gmail.com <github.mutt@gmail.com>
2024-07-24 17:28:10 -0700

    pp

--- topaypal_test.php
+++ topaypal_test.php
@@ -63,6 +63,7 @@ $date = ltrim($dt->format('m/d/Y'),0);
 
 $theTABLE = "mixer_pending";
 
+// going to Paypal
 //echo ("DB INSERT $fname $lname $paid $date $custom");
 
 //toDB($theTABLE, $fname,$lname,$gender,$ntrp,$email, $member,$paid,$date,$custom,$event);
