136bb297d64897eff58190669174db624d76a679
githubmutt@gmail.com <github.mutt@gmail.com>
2024-08-03 14:44:10 -0700

    draw

---
+++ tournament_/womens2.php
@@ -0,0 +1,33 @@
+<html>
+
+   <div class="h3">2024 Summer Mx 6.5 Tournament</div>
+
+ <main id="headers">
+
+ <ul class="round">
+   Quarter-Finals
+   <div class="small">Finish by 9/7/2024</div>
+  </ul>
+  <ul class="round">
+   Semi-Finals
+   <div class="small">Finish by 9/21/2024</div></ul>
+  <ul class="round">
+   Finals
+   <div class="small">Finish by 10/4/2024</div>
+  </ul>
+  
+</main>
+
+<body>
+<main id="tournament2">
+<?php
+  $draw="6.5";
+  $TABLE_TOURNY = "tourny";
+  $YEAR=2024;
+  include "draw.php";
+
+?>
+  <body>
+      
+</body>
+</html>
