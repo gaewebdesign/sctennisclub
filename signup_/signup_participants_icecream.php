0054e11070f8ef0009d68675e07b87a743f5bc58
githubmutt@gmail.com <github.mutt@gmail.com>
2024-08-22 19:13:59 -0700

    ice cream

--- signup_/signup_participants_icecream.php
+++ signup_/signup_participants_icecream.php
@@ -29,8 +29,8 @@ tr {
            <p>
        <?php
           
-          $jurassic = strtotime('2024-7-23');
-          $cretaceous = strtotime('2024-10-7');
+          $jurassic = strtotime('2025-7-23');
+          $cretaceous = strtotime('2025-10-7');
           
           // Switch between tables here **************************
 /*
@@ -40,8 +40,9 @@ tr {
           $query = "select * from ".TABLE_MIXER_OR_PENDING."  where custom>$epoch order by custom desc";
 
 */
-          $query = "select * from ".TABLE_MIXER_PAYPAL."  where custom>$jurassic and custom<$cretaceous order by fname asc";
           $query = "select * from ".TABLE_MIXER_PENDING."  where date>$jurassic and date<$cretaceous order by fname asc";
+          $query = "select * from ".TABLE_MIXER_PAYPAL."  where custom>$jurassic and custom<$cretaceous order by fname asc";
+
 
 //          $query = "select * from ".TABLE_MIXER_PENDING." where date>$jurassic  order by lname";
 
