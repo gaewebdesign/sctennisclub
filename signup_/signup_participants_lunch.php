bc460162fca747036f174b19f55921b98d51dbcf
githubmutt@gmail.com <github.mutt@gmail.com>
2024-08-22 21:03:20 -0700

    parti

--- signup_/signup_participants_lunch.php
+++ signup_/signup_participants_lunch.php
@@ -26,6 +26,9 @@ tr {
            <p>
        <?php
           
+          $jurassic = strtotime('2024-5-10');
+          $cretaceous = strtotime('2024-6-17');
+          
           $epoch = strtotime('2024-5-10');
           // Switch between tables here **************************
           $query = "select * from ".TABLE_MIXER_PENDING." order by custom desc";
@@ -34,7 +37,7 @@ tr {
           $query = "select * from ".TABLE_MIXER_OR_PENDING."  where custom>$epoch order by custom desc";
           $query = "select * from ".TABLE_MIXER_PENDING." where custom>$epoch  order by custom desc";
 
-          $query = "select * from ".TABLE_MIXER_PAYPAL."  where custom>$epoch order by fname asc";
+          $query = "select * from ".TABLE_MIXER_PAYPAL."  where custom>$jurassic and custom<$cretaceous order by fname asc";
 
           $con = Configure();
            
