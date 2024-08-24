eb676cc4d80d1e1e7ef22aed2f826a190061258e
githubmutt@gmail.com <github.mutt@gmail.com>
2024-08-24 01:15:45 -0700

    notify.php

---
+++ teamtennis_/cancel.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SCTC</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/checkout/">
+
+
+
+<link href="./assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+
+</head>
+<body>
+    <p>
+
+    <p> &nbsp;<p> &nbsp;
+    <p> &nbsp;
+    <p> &nbsp;
+    <center>
+    <h1> Next time! </h1>
+    <p>
+    <div class="col d-flex align-items-center justify-content-center">
+
+    <!--<img src="./cinco.png"  alt="cinco"> -->
+    <!--
+    https://myanimelist.net/featured/2039/Anime_Girls_Crying__20_of_the_Saddest_Pictures___GIFs
+
+    <img src="./images/crying.gif" alt="crying">
+   <img src = "./images/naomi-osaka-tennis.gif" >  
+
+   <img src="./images/shiro-neko-project-tennis.gif" >    
+   <div class="tenor-gif-embed" data-postid="22493637" data-share-method="host" data-aspect-ratio="1" data-width="75%"><a href="https://tenor.com/view/carlos-alcaraz-tennis-fancy-footwork-espana-tenis-gif-22493637">Carlos Alcaraz Tennis GIF</a>from <a href="https://tenor.com/search/carlos+alcaraz-gifs">Carlos Alcaraz GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script> 
+<div class="tenor-gif-embed" data-postid="22493637" data-share-method="host" data-aspect-ratio="1" data-width="75%"><a href="https://tenor.com/view/carlos-alcaraz-tennis-fancy-footwork-espana-tenis-gif-22493637">Carlos Alcaraz Tennis GIF</a>from <a href="https://tenor.com/search/carlos+alcaraz-gifs">Carlos Alcaraz GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script> 
+    -->
+
+
+    
+
+    <div class="tenor-gif-embed" data-postid="24648674" data-share-method="host" data-aspect-ratio="1.10345" data-width="70%"><a href="https://tenor.com/view/iga-swiatek-slide-backhand-tennis-polska-gif-24648674">Iga Swiatek Slide GIF</a>from <a href="https://tenor.com/search/iga+swiatek-gifs">Iga Swiatek GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>    
+    </div>
+
+</body>
+</html>
---
+++ teamtennis_/notify.php
@@ -0,0 +1,95 @@
+<?php
+
+
+include "../library/include.inc";
+
+//$_GET["custom"] = 1675506456;;
+
+//LOGGER("notify.php called custom:" + $_GET["custom"]);
+//LOG_EMAIL("notify.php called  with custom:".$_GET["custom"]);
+
+
+
+LOGGER("notify.php called : _GET custom = " .$_GET["custom"]);
+LOGGER("notify.php called : _POST custom = " .$_POST["custom"]);
+LOGGER("notify.php  Use _POST !!!! ******************");
+
+
+LOGGER("notify.php: enumerate _GET array");
+TEXT("notify.php: enumerate _GET array");
+foreach ($_GET as $key => $value) {
+   LOGGER("notify.php $key -> $value ");
+   TEXT("notify.php $key -> $value ");
+
+}
+
+
+
+TEXT("notify.php: enumerate _POST array");
+LOGGER("notify.php: enumerate _POST array");
+foreach ($_POST as $key => $value) {
+   LOGGER("notify.php $key -> $value ");
+   TEXT("notify.php $key -> $value ");
+}
+
+if( !empty($_POST["custom"])){
+
+   $epoch = $_POST["custom"];
+   LOGGER("notify.php called  with POST custom: $epoch ");
+   
+   $dest = TABLE_MIXER_PAYPAL;
+   $src = TABLE_MIXER_PENDING;
+
+   // copy over to mixer (paypal contents)
+   LOGGER("notify.php saving Mixer information from pending to mixer copyto $src -> $dest : $epoch " );
+// copyto_mixer( $src, $dest, $epoch );
+   copyto( $src, $dest, $epoch);
+
+   // notify email
+   $data = array(
+      'year' => YEAR,
+      'fname' => "-",
+      'lname' => "-",
+      'custom' => $epoch,
+      'src' => $src,
+      'dest' => $dest,
+      'subject' => "signup_/notify.php: $src to $dest (copyto paypal )",
+      'message' => "signup_/notify.php: $src to $dest (copyto paypal )"
+   );
+   
+   SENDER( $data );
+
+
+
+
+
+
+ 
+}
+
+// using the 'custom' number (i.e. time() generated)
+// this moves member from pending to mixer table
+// and sends email to santaclarawebmaster@gmail.com
+$subject = "Mixer";
+$name = "Capt Kirk";
+
+TEXT("EMAILER");
+EMAILER( $subject, $name, $verbose=true);
+
+//emailer(true);
+
+function LOG_EMAIL($to , $message){
+   $to = "tennis.mutt@gmail.com";
+   $subject = "LOGR: $message";
+
+   $headers  = 'MIME-Version: 1.0' . "\r\n";
+   $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+   $headers .= "From: membership@sctennisclub.org  \r\n";
+ 
+   $r=mail($to,$subject,$message,$headers); 
+
+}
+
+
+
+?>
