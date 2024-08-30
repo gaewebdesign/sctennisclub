a31f8d481461febccb60f13c1919a3732f37b742
githubmutt@gmail.com <github.mutt@gmail.com>
2024-08-30 01:10:03 -0700

    email

--- ladder_/ladder_email_send.php
+++ ladder_/ladder_email_send.php
@@ -35,24 +35,56 @@ if( $gender=="F") {
     $title = "Women's Singles Participants";
 }
 
-if($found ){
+  
+   if($found)
+      emailtable($con,$theTABLE,$title, $division);
+
+
+
+function emailtable($con, $theTABLE,$title,$division){
+    // 
+    echo ('<html>' );
+    echo ('<style>');
+    echo ('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">');
+    echo ('</style>');
+    
+//    echo('<body>');
+    echo("<center>");
+    echo("<p><br>");
+    echo("<h2>$title</h2");
+    echo ("<p><hr>");
+
+
+    echo('<table style="font-size: 24px">');
+    
+    echo ('<thead>');
+    echo ('<tr>');
+    echo ('<td>');
+
+    echo ('</td>');
+    echo ('</tr>');
+    echo ('</thead>');
+
     $query = "select * from $theTABLE where division=\"$division\" ";
     $qr=mysqli_query($con,$query);
 
-    echo ("<center>");
-    echo("<h1>$title</h1><p>");
 
     while( $row = mysqli_fetch_assoc($qr)){
+        echo("<tr>");
+        echo("<td>");
+        echo( $row["fname1"] );
+        echo(" ");
+        echo( $row["lname1"]);
+        echo("<td>");
+        echo( $row["email1"] );
 
-        echo("<h2>");
-        echo $row["fname1"]." ".$row["lname1"]." ". $row["email1"]."<br>";
-        echo("</h2>");
     }
-    echo ("</center>");
-    
+
+    echo ('</tbody> ');
+    echo("</table>");
+
+
 }
-
-
 
 
 
