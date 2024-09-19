24ba7488f05bbc5a971314d21ba703fbe3b98c39
githubmutt@gmail.com <github.mutt@gmail.com>
2024-09-18 23:20:07 -0700

    pigout

--- topigout.php
+++ topigout.php
@@ -100,52 +100,26 @@ else{
 
 //sendemail($fname." ".$lname, $email, "sctennisclub.org => $theTABLE ");
 
+
 $subject = "Pigout Signup ($fname $lname) ---";
 $email = "santaclarawebmaster@gmail.com";
 $message = "Pigout 2024 $fname $lname signup";
 
-  phpemailer($subject, $message , "tennis.mutt@gmail.com",$email);
 
-// Completely Automated Public Turing Test to tell Computers and Humans Aparat
-function checkCAPTCHA() {
+// Thanks for Signing up!
+	echo "<script>alert('$message');</script>";
+	echo('
+	<script >
+		window.setTimeout(function() {
+					window.location.href="./signup_free.phtml";
+			}, 500);
+	</script>
+	');
 
-    // Checking valid form is submitted or not 
-if (isset($_POST['SubmitButton'])) { 
-	
-	// Storing name in $name variable 
-	$name = $_POST["fname"]."  " .$_POST["lname"]; 
-	
-	// Storing google recaptcha response 
-	// in $recaptcha variable 
-	$recaptcha = $_POST['g-recaptcha-response']; 
+// Thanks for Signing up!
 
-	// Put secret key here, which we get 
-	// from google console 
-	$secret_key = '6LdoCf4pAAAAAArgp6FUOA7Rn0j7_Jle-2dL-cUF'; 
+phpemailer($subject, $message , "tennis.mutt@gmail.com",$email);
 
-	// Hitting request to the URL, Google will 
-	// respond with success or error scenario 
-	$url = 'https://www.google.com/recaptcha/api/siteverify?secret='
-		. $secret_key . '&response=' . $recaptcha; 
-
-	// Making request to verify captcha 
-	$response = file_get_contents($url); 
-
-	// Response return by google is in 
-	// JSON format, so we have to parse 
-	// that json 
-	$response = json_decode($response); 
-
-	// Checking, if response is true or not 
-	if ($response->success == true) { 
-        
-        echo '<script>alert("Summer 2024 \nSanta Clara Tennis Club \n Pigout  \nYou\'re signed up!")</script>'; 
-        return true;
-	} else { 
-		echo '<script>alert("Fill out the  the  reCAPTACHA")</script>'; 
-        return false;
-	} 
-} 
 
 
 
