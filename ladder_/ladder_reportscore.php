6d4c83fb34709741bb6a74617245a7bdce7dbb8d
githubmutt@gmail.com <github.mutt@gmail.com>
2024-10-05 01:35:41 -0700

    report score

--- ladder_/ladder_reportscore.php
+++ ladder_/ladder_reportscore.php
@@ -207,7 +207,9 @@ function between($x , $min,$max){
 }
 
 function announce_score($fname1,$lname1,$email1,$fname2,$lname2, $points, $w_add , $l_add,$score,$bonus1,$bonus2,$date ){
-
+   echo("<style>");
+   echo("</style>");
+   echo("<body bgcolor=\"CAE9F5\" >");
    echo("<center>");
    echo("<h1>Score Report </h1>");
    echo("<h3>");
@@ -239,135 +241,12 @@ function announce_score($fname1,$lname1,$email1,$fname2,$lname2, $points, $w_add
    echo("</h3>");   
 
    echo("</center>");
-}
-
-
-function reportScore_($theTABLE,$division,$winner_id,$loser_id,$score){
-
-    $con = DBMembership();
-
-     $custom = get_custom($winner_id);
-     $_id = $winner_id;
- 
-     $query1 = "update $theTABLE set payment=\"ERR\" where _id=$_id";     
-     $query2 = "update $theTABLE set payment=\"ERR\" where _id=$_id";     
-     $query3 = "update $theTABLE set custom=custom+1 where _id=$_id";     
-
-     $_id = $winner_id;
-     switch($custom){
-         case 0:
-            $query1 = "update $theTABLE set semis=\"semis\" where _id=$_id";
-            $query2 = "update $theTABLE set score1=\"$score\" where _id=$_id";
-         break;
-         case 1:
-            $query1 = "update $theTABLE set finals=\"finals\" where _id=$_id";
-            $query2 = "update $theTABLE set score2=\"$score\" where _id=$_id";
-         break;
-         case 2:
-            $query1 = "update $theTABLE set winner=\"winner\" where _id=$_id";
-            $query2 = "update $theTABLE set score3=\"$score\" where _id=$_id";
-         break;
-
-         default:
-         
-
-     }
-
-/*
-     echo("custom = $custom <br>");
-     echo("$query1 <br>");
-     echo("$query2 <br> ");
-     echo("$query3  <br>");
-*/
-     LOGGER("TOURNY: $query1 ");
-     LOGGER("TOURNY: $query2 ");
-     LOGGER("TOURNY: $query3 ");
-
-     
-          $query_results=mysqli_query($con, $query1);
-          $query_results=mysqli_query($con, $query2);
-          $query_results=mysqli_query($con, $query3);
-         
-
-
-   // Update Opponent  ***********************************************
-   // just need to put LOSS in
-    $_id = $loser_id;
-    $custom = get_custom($loser_id);
-    $query = "update $theTABLE set payment=\"ERR\" where _id=$_id";     
-    $query=$query1=$query2="";
-    switch($custom){
-        case 0:
-           $query1 = "update $theTABLE set semis=\"Loss\" where _id=$_id";
-           $query2 = "update $theTABLE set custom=\"99\" where _id=$_id";
-           break;
-        case 1:
-           $query1 = "update $theTABLE set finals=\"Loss\" where _id=$_id";
-           $query2 = "update $theTABLE set custom=\"99\" where _id=$_id";
-           break;
-        case 2:
-           $query1 = "update $theTABLE set winner=\"Loss\" where _id=$_id";
-           $query2 = "update $theTABLE set custom=\"99\" where _id=$_id";
-        break;
-
-        default:
-//          echo("custom = $custom");
-
-
-    }
-/*
-    echo "custom = $custom <br>";
-    echo $query1."<br>";
-    echo $query2."<br>";
-*/
-    $query_results=mysqli_query($con, $query1);
-    $query_results=mysqli_query($con, $query2);
-    LOGGER("TOURNY: $query1 (UPDATE LOSER round to Loss");
-    LOGGER("TOURNY: $query2 (UPDATE LOSER custom to 99");
-    LOGGER("TOURNY: ---");
-
-//    echo $query;
- 
- }
-
-
-
-
-function get_mtype($division ){
-
-   $con = DBMembership();
-   $query ="SELECT MAX(mtype) FROM tourny where division=\"$division\"";
-   
-   $qr = mysqli_query($con, $query);
-   $row = mysqli_fetch_array($qr);
-
-   $mtype =  0; 
-   if( is_array($row)) $mtype =  $row[0]; 
-
-   // return one higher than the current mtype
-   return $mtype+1;
-
+   echo("</body>");
 
 }
 
-function get_custom($_id){
 
-    $con = DBMembership();
-    $query ="SELECT custom FROM tourny where _id=\"$_id\"";
-    
-    $qr = mysqli_query($con, $query);
-    $row = mysqli_fetch_array($qr);
- 
-    $custom =  0; 
-    if( is_array($row)) $custom =  $row[0]; 
- 
-    // return custom value whichi get increment after each win
-    // 0: in quarterfinals (start)
-    // 1: in semi-final
-    // 2: in finals
-    return $custom;
 
-}
 
 function password($fname1,$fname2){
     $date = "".time()-60*60*7;
@@ -391,80 +270,6 @@ function password($fname1,$fname2){
 
 }
 
-function enterTournament($theTABLE,$fname1,$lname1,$email1,$gender1,$ntrp1,$fname2,$lname2,$email2,$gender2,$ntrp2,$year,$division,$team,$mtype,$pwd,$date){
 
-    $con = DBMembership();
-
-    $query = 'insert into '.$theTABLE.'(_id,fname1,lname1,email1,gender1,ntrp1,fname2,lname2,email2,gender2,ntrp2,year,division,team,mtype,pwd,date)';
-   
-    $query .= ' values (NULL'.add($fname1).add($lname1).add($email1).add($gender1).add($ntrp1);
-    $query .= add($fname2).add($lname2).add($email2).add($gender2).add($ntrp2);
-    $query .= add($year).add($division).add($team).add($mtype).add($pwd).add($date);
-    $query .= ")";
- 
- 
-    $query_results=mysqli_query($con, $query);
- 
- }
-
-function toTournyDB($theTABLE,$fname1,$lname1,$email1,$gender1,$ntrp1,$fname2,$lname2,$email2,$gender2,$ntrp2,$year,$division,$team,$mtype,$date,$insignia,$payment,$custom,$opt,$pwd){
-
-   $con = DBMembership();
-
-   $query = 'insert into '.$theTABLE.'(_id,fname1,lname1,email1,gender1,ntrp1,fname2,lname2,email2,gender2,ntrp2,year,division,team,mtype,date,insignia,payment,custom,opt,pwd)';
-  
-   $query .= ' values (NULL'.add($fname1).add($lname1).add($email1).add($gender1).add($ntrp1);
-   $query .= add($fname2).add($lname2).add($email2).add($gender2).add($ntrp2);
-   $query .= add($year).add($division).add($team).add($mtype).add($date);
-   $query .= add($insignia).add($payment).add($custom).add($opt).add($pwd);
-   $query .= ")";
-
-//   LOGGER( $query );
-//   echo "<p>";
-//   echo $query;
-
-   $query_results=mysqli_query($con, $query);
-
-}
-
-
-function signedUp($theTABLE,$fname1,$lname1,$email1,$gender1,$ntrp1,$fname2,$lname2,$email2,$gender2,$ntrp2,$year,$division,$team,$mtype,$date,$insignia,$payment,$custom,$opt,$pwd){
-
-
-    echo("<h1>Signed Up </h1> " );
-    echo("<h3>");
-    echo("$fname1 $lname1   / $fname2 $lname2   $division");
-    echo("</h3>");
-
-
-}
 ?>
 
-<!--
-| Field    | Type        | Null | Key | Default | Extra          |
-+----------+-------------+------+-----+---------+----------------+
-| _id      | int         | NO   | PRI | NULL    | auto_increment |
-| fname1   | varchar(31) | YES  |     | NULL    |                |
-| lname1   | varchar(31) | YES  |     | NULL    |                |
-| email1   | varchar(50) | YES  |     | NULL    |                |
-| gender1  | char(1)     | YES  |     | NULL    |                |
-| ntrp1    | varchar(5)  | YES  |     | NULL    |                |
-| fname2   | varchar(31) | YES  |     | NULL    |                |
-| lname2   | varchar(31) | YES  |     | NULL    |                |
-| email2   | varchar(50) | YES  |     | NULL    |                |
-| gender2  | char(1)     | YES  |     | NULL    |                |
-| ntrp2    | varchar(5)  | YES  |     | NULL    |                |
-| year     | varchar(40) | YES  |     | NULL    |                |
-| division | varchar(31) | YES  |     | NULL    |                |
-| team     | varchar(31) | YES  |     | NULL    |                |
-| mtype    | varchar(31) | YES  |     | NULL    |                |
-| date     | int         | YES  |     | NULL    |                |
-| insignia | varchar(7)  | YES  |     | NULL    |                |
-| payment  | varchar(16) | YES  |     | NULL    |                |
-| custom   | varchar(31) | YES  |     | NULL    |                |
-| opt      | varchar(31) | YES  |     | NULL    |                |
-| pwd      | varchar(31) | YES  |     | NULL    |                |
-+----------+-------------+------+-----+---------+----------------+
-21 rows in set (0.00 sec)
-
--->
