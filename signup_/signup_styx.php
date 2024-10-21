00fa8d0c688222deb6ac9f21730b13cd1922ce58
Roger Okamoto <ro@Rogers-Mac-mini.attlocal.net>
2024-10-21 15:55:16 -0700

    new date

--- signup_/signup_styx.php
+++ signup_/signup_styx.php
@@ -74,7 +74,7 @@
 
     <h3 style="font-size: 1.2em ">
     Enjoy dinner at <a href="http://www.chinastix.net"><b>China Stix</b></a> in Santa Clara on Thursday November 14 , 6-8 pm. (dinner served at 6:30)
-     Early bird registration is $<?php echo($MEMBER_FEE) ?> until October 21, 2024.
+     Early bird registration is $<?php echo($MEMBER_FEE) ?> until October 28, 2024.
     Registration afterwards will be $25 until the final deadline of November 4 <br/>
     Signup page is at <a href="./signup">www.sctennisclub.org/signup</a>
   
