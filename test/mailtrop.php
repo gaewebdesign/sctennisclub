7ad9a5b16b62585a1c2430e1e0c70c53cb813604
githubmutt@gmail.com <github.mutt@gmail.com>
2024-10-29 15:21:37 -0700

    mailtrop

---
+++ test/mailtrop.php
@@ -0,0 +1,39 @@
+<?php
+
+$to = 'tennis.mutt@gmail.com';
+$from = 'rogero.tennis@gmail.com';
+$subject = 'Hello!';
+
+$headers['From'] = 'sender@example.com';
+$headers['MIME-Version'] = 'MIME-Version: 1.0';
+$headers['Content-type'] = 'text/html; charset=iso-8859-1';
+
+$message = '
+<html>
+<head>
+    <title>Review Request Reminder</title>
+</head>
+<body>
+    <p>Here are the cases requiring your review in December:</p>
+    <table>
+        <tr>
+            <th>Case title</th><th>Category</th><th>Status</th><th>Due date</th>
+        </tr>
+        <tr>
+            <td>Case 1</td><td>Development</td><td>pending</td><td>Dec-20</td>
+        </tr>
+        <tr>
+            <td>Case 2</td><td>DevOps</td><td>pending</td><td>Dec-21</td>
+        </tr>
+    </table>
+</body>
+</html>
+';
+
+$result = mail($to, $subject, $message, $headers);
+
+if ($result) {
+    echo 'Success!' . PHP_EOL;
+} else {
+    echo 'Error.' . PHP_EOL;
+}
