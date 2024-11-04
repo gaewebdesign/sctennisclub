75d847a18f7634f7291c12a38c87ff130efc7fc6
githubmutt@gmail.com <github.mutt@gmail.com>
2024-11-03 18:35:10 -0800

    signup_styx 25

--- signup_/signup_styx.php
+++ signup_/signup_styx.php
@@ -58,7 +58,7 @@
   <?php 
    // include "includes.inc";
    $MEMBER_FEE=DINNER;
-   $MEMBER_FEE="20";
+   $MEMBER_FEE="25";
   
   ?>
 
@@ -74,8 +74,8 @@
 
     <h3 style="font-size: 1.2em ">
     Enjoy dinner at <a href="http://www.chinastix.net"><b>China Stix</b></a> in Santa Clara on Thursday November 14 , 6-8 pm. (dinner served at 6:30)
-     Early bird registration is $<?php echo($MEMBER_FEE) ?> until October 28, 2024.
-    Registration afterwards will be $25 until the final deadline of November 4 <br/>
+     Early bird registration is $<?php echo($MEMBER_FEE-5) ?> until October 28, 2024.
+    Registration afterwards will be $<?php echo($MEMBER_FEE) ?> until the final deadline of November 4 <br/>
     Signup page is at <a href="./signup">www.sctennisclub.org/signup</a>
   
     </h3>
