1d41740fb4ce86875b7d74101574d7034709e9af
githubmutt@gmail.com <github.mutt@gmail.com>
2024-11-08 21:09:46 -0800

    disable join

--- join_/joinsctc.php
+++ join_/joinsctc.php
@@ -172,6 +172,6 @@ include "./library/emailer.php";
   </div>
   <div class="col-12"> &nbsp;</div>
   <div class="col-12">
-    <button class="btn btn-primary"  type="submit">Go to Paypal</button>
+    <button class="btn btn-primary" disabled  type="submit">Go to Paypal</button>
   </div>
 </form>
