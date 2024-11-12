975da71fff4a7a5c96b06972702fc994aa211b10
githubmutt@gmail.com <github.mutt@gmail.com>
2024-11-12 01:54:39 -0800

    teamt

--- topaypal_teamtennis.php
+++ topaypal_teamtennis.php
@@ -104,7 +104,7 @@ $data = array(
 
 $subject= "2024 Team Tennis (pending)";
 $rating = $gender.$ntrp;
-$message = "$fname $lname \n$rating \n$email \n$phone ";
+$message = "$fname $lname <br>$rating <br>$email <br>$phone ";
 
 $recipient = "santaclarawebmaster@gmail.com";
 phpemailer($subject, $message , $recipient);
