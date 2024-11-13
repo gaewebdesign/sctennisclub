46aa5092919285dbc465937a3840a467fe117ca7
githubmutt@gmail.com <github.mutt@gmail.com>
2024-11-12 17:17:30 -0800

    paid table

--- teamtennis_/teamtennis_enter.php
+++ teamtennis_/teamtennis_enter.php
@@ -76,8 +76,8 @@
 
     <h3 style="font-size: 1.2em ">
     Enjoy team tennis  at <b>Santa Clara Tennis Center</b> 
-    on Sunday December 8,2024  1:30pm - 4:30 pm. Deadline to signup is December 1 or until the event is filled.
-    Cost is $<?php echo($MEMBER_FEE )?>for members , $ <?php echo($GUEST_FEE) ?> for guests <br/>
+    on Sunday December 8,2024  1:30 pm - 4:30 pm. Deadline to signup is December 1 or until the event is filled.
+    Cost is $<?php echo($MEMBER_FEE) ?> for members, $<?php echo($GUEST_FEE); ?> for guests. <br/>
     Signup page is at <a href="./signup">www.sctennisclub.org/teamtennis</a>
     <p>
       For more information email Roy and Alice at <br>
