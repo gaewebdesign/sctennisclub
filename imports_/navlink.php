36d0d53ca9529cd494f6890f8150aed5e16af1e3
githubmutt@gmail.com <github.mutt@gmail.com>
2024-11-15 00:48:10 -0800

    index

--- imports_/navlink.php
+++ imports_/navlink.php
@@ -96,10 +96,11 @@ https://getbootstrap.com/docs/5.1/getting-started/introduction/
 
               <li><a class="dropdown-item" href="./signup_lunch.phtml">June - Walk like an Egyptian</a></li>
               <li><a class="dropdown-item" href="./signup_free.phtml">July - Annual Pigout</a></li>
-              <li><hr class="dropdown-divider" style="border-color:black;"></li>
+              
               <li><a class="dropdown-item" href="./signup_icecream.phtml">August - Ice Cream Social</a></li>
               <li><a class="dropdown-item" href="./signup_styx.phtml">November - End of year Dinner</a></li>
               <li><a class="dropdown-item" href="./teamtennis.phtml">December - Team Tennis</a></li>
+              <li><hr class="dropdown-divider" style="border-color:black;"></li>
 
 <!--
               <li><a class="dropdown-item" href="./signup_styx.phtml">November - Annual Dinner</a></li>
