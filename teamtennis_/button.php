9deaf7dd319ec8aac5600321412e7663aedd8745
githubmutt@gmail.com <github.mutt@gmail.com>
2024-11-23 18:30:43 -0800

    button

---
+++ teamtennis_/button.php
@@ -0,0 +1,3 @@
+<button class="w-500 btn btn-primary btn-lg"   name="SubmitButton" > Signup for Team Tennis! </button>
+
+
---
+++ teamtennis_/button_disabled.php
@@ -0,0 +1,4 @@
+<button class="w-500 btn btn-primary btn-lg"  disabled  name="SubmitButton" > Entries are 
+    full -- see you on the courts! </button>
+
+
