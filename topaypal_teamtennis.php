9b7c1923b4dd06e5529bcfd7a44bdbdc5d4bff54
githubmutt@gmail.com <github.mutt@gmail.com>
2024-11-28 19:22:44 -0800

    tt deadline

--- topaypal_teamtennis.php
+++ topaypal_teamtennis.php
@@ -105,7 +105,7 @@ $data = array(
 $subject= "2024 Team Tennis (late entry  -- $"."$payment at desk)";
 $rating = $gender.$ntrp;
 $message = "LATE ENTRY: if there are openings, contact this person <br> ";
-$message = "$fname $lname <br>$rating <br>$email <br>$phone ";
+$message .= "$fname $lname <br>$rating <br>$email <br>$phone ";
 $recipient = "rogie@sctennisclub.org";
 
 $res = CHECK_CAPTCHA() ;
