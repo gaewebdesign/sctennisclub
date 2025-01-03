ec3211b219c8782331d27670db38bd7559f3c798
githubmutt@gmail.com <github.mutt@gmail.com>
2025-01-03 02:20:00 -0800

    test

---
+++ test_/test.php
@@ -0,0 +1,26 @@
+ 
+
+<?php
+
+include "../library/include.inc";
+
+$year = 1999;
+$fname = "Kareem";
+$lname = "Abdul-Jabbar";
+$email = "jbbar@gmail.com";
+$address = "Fabulous Forum";
+$city = "Las Vegas";
+$pwd = Password();
+$mtype="RF";
+$account = time();//"5552342233";
+$count = 1;
+$trust = 0;
+
+
+toFamilyDB($year,$fname,$lname,$email,$address,$city,$pwd,$mtype,$account,$count,$trust);
+
+
+
+
+
+?>
