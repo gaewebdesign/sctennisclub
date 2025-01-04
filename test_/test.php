79273b8ed668be3110c00248f7ec908fa1d5c0cc
githubmutt@gmail.com <github.mutt@gmail.com>
2025-01-03 21:36:13 -0800

    incrementFamilyCount

--- test_/test.php
+++ test_/test.php
@@ -4,23 +4,39 @@
 
 include "../library/include.inc";
 
-$year = 2025;
-$fname = "Kareem";
-$lname = "Abdul-Jabbar";
-$email = "jbbar@gmail.com";
-$address = "Fabulous Forum";
-$city = "Las Vegas";
-$pwd = Password();
-$mtype="RF";
-$account = time();//"5552342233";
-$count = 1;
-$trust = 0;
+function loadFamilyDB(){
+
+    $year = 2025;
+    $fname = "Kareem";
+    $lname = "Abdul-Jabbar";
+    $email = "jbbar@gmail.com";
+    $address = "Fabulous Forum";
+    $city = "Las Vegas";
+    $pwd = Password();
+    $mtype="RF";
+    $account = time();
+    $count = 1;
+    $trust = 0;
 
 
-toFamilyDB($year,$fname,$lname,$email,$address,$city,$pwd,$mtype,$account,$count,$trust);
+    toFamilyDB($year,$fname,$lname,$email,$address,$city,$pwd,$mtype,$account,$count,$trust);
+}
+
+/*
+function incrementFamilyCount( $address,$year){
+
+    $theTable = TABLE_FAMILY;
+
+    $query = "update $theTable set count = count + 1 where address = \"$address\" and year=$year " ;
+
+    $con = DBMembership();
+    $query_results=mysqli_query($con,$query);
+
+}
+*/
 
 
-
+incrementFamilyCount("PO Box 88" , 2025 );
 
 
 ?>
