e76fab4456ebb6cfce962ebc567bfa44761d6d06
githubmutt@gmail.com <github.mutt@gmail.com>
2025-01-28 02:08:32 -0800

    cron

--- join_/cronwait.php
+++ join_/cronwait.php
@@ -4,18 +4,40 @@ include "../library/paypal.inc";
 include "../library/emailer.php";
 include "../library/email/email.inc";
 
-$fname = "Roger";
-$lname = "Okamoto";
-$address = "from join _directory";
-$email   = "sctc@sctennisclub.org";
+
+$src = "waitlist"; //TABLE_WAITLIST;
+$dest = "temporary";
+
+$con = DBMembership();
+$query = "select * from $src where custom != \"done\" order by date asc limit 1";
+
+//echo $query;
+
+$qr=mysqli_query($con,$query);
+
+$fname=$lname=$email=$address=$mtype= $epoch="";
+while ($row = mysqli_fetch_assoc($qr)) {  
+    $epoch = $row[CUSTOM];
+    $fname = $row[FNAME];
+    $lname = $row[LNAME];
+    $email = $row[EMAIL];
+    $address = $row[ADDRESS];
+    $mtype = $row[MTYPE];
+
+    echo("COPY $fname $lname $email $mtype   -- @ $epoch \n");
+//    copyto( $src, $dest, $epoch);
+
+}
 
 
 $subject= "2025 Waitlist Cron Job ( $fname $lname)";
 $message = "CRON Waitlist Check<br> ";
 $message .= "$fname $lname <br>$address<br>$email <br>";
+$message .= "$mtype <br>$epoch <br>";
 $recipient = "mutt@sctennisclub.org";
 phpemailer($subject,$message , $recipient , "south@sctennisclub.org");
 
 
-echo ("sending $subject");
+echo ("<br>sending $message");
+
 ?>
