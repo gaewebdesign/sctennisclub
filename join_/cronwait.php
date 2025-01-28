2e3c8c55f474ce241ba2abf4f3a5a2b04cefb4df
githubmutt@gmail.com <github.mutt@gmail.com>
2025-01-27 23:01:03 -0800

    wait list job

---
+++ join_/cronwait.php
@@ -0,0 +1,21 @@
+<?php
+include "../library/include.inc";
+include "../library/paypal.inc";
+include "../library/emailer.php";
+include "../library/email/email.inc";
+
+$fname = "Roger";
+$lname = "Okamoto";
+$address = "380 Santa Cruz Ave";
+$email   = "sctc@sctennisclub.org";
+
+
+$subject= "2025 Waitlist Cron Job ( $fname $lname)";
+$message = "CRON Waitlist Check<br> ";
+$message .= "$fname $lname <br>$address<br>$email <br>";
+$recipient = "mutt@sctennisclub.org";
+phpemailer($subject,$message , $recipient , "south@sctennisclub.org");
+
+
+
+?>
