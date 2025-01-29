f53ffd7ecdc9ef4a48b137aaf940a8f7d24e7e9d
githubmutt@gmail.com <github.mutt@gmail.com>
2025-01-29 00:30:52 -0800

    cronwait

--- join_/cronwait.php
+++ join_/cronwait.php
@@ -7,12 +7,34 @@ include "../library/email/email.inc";
 $src = "waitlist"; //TABLE_WAITLIST;
 $dest = "temporary";
 
+
 $con = DBMembership();
 
 $query = "select * from $src where custom != \"done\" order by date asc limit 1";
 echo $query."<br>";
 
+$mutt  = "mutt@sctennisclub.org";
+$south = "south@sctennisclub.org";
+
 $qr=mysqli_query($con,$query);
+if( mysqli_num_rows($qr) == 0){
+
+    $subject= "2025 Waitlist Cron Job ";
+    $message = "No one on waitlist";
+    phpemailer($subject,$message , $mutt , $south );
+    echo("cronwait.php:  no one on wait list");
+    LOGS("cronwait.php:  no one on wait list");
+    return;
+}
+
+if(ResidentMajority(YEAR) == false){
+    $subject= "2025 Waitlist Cron Job";
+    $message = "Unable to move to waitlist";
+    phpemailer($subject,$message , $mutt , $south );
+    echo("cronwait.php:  not enough space to make member");
+    LOGS("cronwait.php:  not enough space to make member");
+    return;
+}
 
 $fname=$lname=$email=$address=$mtype= $epoch="";
 while ($row = mysqli_fetch_assoc($qr)) {  
@@ -25,17 +47,16 @@ while ($row = mysqli_fetch_assoc($qr)) {
 
     echo("COPY $fname $lname $email $mtype   -- @ $epoch to $dest \n");
     LOGS("COPY $fname $lname $email $mtype   -- @ $epoch to $dest \n");
-
- //  copyto( $src, $dest, $epoch);
-
 }
+
+    copyto( $src, $dest, $epoch);
+
 
 $subject= "2025 Waitlist Cron Job ( $fname $lname)";
 $message = "CRON Waitlist Check<br> ";
 $message .= "$fname $lname <br>$address<br>$email <br>";
 $message .= "$mtype <br>$epoch <br>";
-$recipient = "mutt@sctennisclub.org";
-phpemailer($subject,$message , $recipient , "south@sctennisclub.org");
+phpemailer($subject,$message , $mutt , $south);
 
 echo ("<br>sending $message");
 
