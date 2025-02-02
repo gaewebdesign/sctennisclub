2e2be1b8f89ce1f44590b9d0fa110be43b670de8
githubmutt@gmail.com <github.mutt@gmail.com>
2025-02-01 20:49:28 -0800

    wait ui

--- tomemb_fromwaitlist.php
+++ tomemb_fromwaitlist.php
@@ -12,6 +12,10 @@ echo "<p><br>";
 //echo "<h3>";
 
 //print_r( $_POST );
+$theYear = YEAR;
+$dest = TABLE_PAYPAL;
+$source = TABLE_WAITLIST;
+$destination = TABLE_TEMPORARY;
 
 if( false == isset($_POST["_id"])  ){
     echo("Select someone from waitlist table");
@@ -20,8 +24,8 @@ if( false == isset($_POST["_id"])  ){
    
 
 if ( ResidentMajority(YEAR) != true){
-    echo("ResidentMajority is false cannot add to membership ");
-    return;
+    echo("ResidentMajority is false cannot add to membership (disabled) ");
+//    return;
 
 }
 
@@ -39,6 +43,7 @@ foreach ($_POST["_id"] as $key => $value){
     $mtype = $row[MTYPE];
     $address = $row[ADDRESS];
     $custom = $row[CUSTOM];
+    $email = $row[EMAIL];
 
     if( $custom == "done" ){
         echo( "skipping over $fname $lname , already done " );
@@ -48,10 +53,22 @@ foreach ($_POST["_id"] as $key => $value){
     //  $query = "update $theTable set trust =$trust where  _id=$_id";
     $query = "copy query  where  _id=$_id";
     echo "$fname $lname $address $mtype <br/>";
-    $source = TABLE_WAITLIST;
-    $destination = TABLE_PAYPAL;
-    echo "COPYING to $source from $destination <br/>";
-    copyto( TABLE_WAITLIST,  TABLE_PAYPAL, $custom);
+        
+    echo "COPYING from $source to $destination <br/>";
+    copyto( $source, $destination, $custom);
+
+// SEND EMAIL TO PLAY  ************************************
+    $subject= "$theYear Santa Clara Tennis Club( $fname $lname)";
+    $message = "CRON Waitlist Check<br> ";
+    $message = "";
+    $message .= notifyplayer($fname, $lname);
+
+    echo($message);
+
+    LOGS("cronwait.php: moving from $src to $dest ");
+    phpemailer($subject,$message , $email, $south);
+
+// ********************************************************
 
 //  $query = "update $theTable set trust =$trust where  _id=$_id";
  
@@ -62,4 +79,55 @@ echo "</center>";
 echo "</h3>";
 
 
+function notifyplayer($fname,$lname){
+
+    //    <th style="width:10%">Sel</th>
+        $retv = "";
+        $retv.= "<!DOCTYPE html>";
+        $retv.= ('<body style="background-color:powderblue;"> ');
+        $retv.=  "<table>";
+        $retv.=  "<thead>";
+        $retv.=  "<tr>";
+    
+        $retv.=  "<th style=\"width:10%\" > </th> ";
+        $retv.=  "<th style=\"width:85%\" > </th> ";
+        $retv.=  "<th style=\"width:5%\" > </th> ";
+    
+        $retv.=  "<tr>";
+        $retv.=  "</thead>";
+        $retv.=  "<tr><td><td>";
+        $retv.=  "<h3>$fname $lname </h3><td>";
+        
+        $retv.=  "<tr><td><td>";
+        $retv.=  "<h3>Welcome to Santa Clara Tennis Club</h3>";
+        $retv.=  "<h4>You are an official SCTC member</h4>";
+    
+        $retv.=  "<h4>You may participate in all activities as a SCTC member</h4>";
+        $retv.=  "<td></tr>";
+    
+        $retv.= "<tr><td><td>";
+        $retv.=  "<h4>Including: </h4>";
+        $retv.=  "</tr>";
+    
+        $retv.=  "<tr><td><td>";
+        $retv.=  "<h4>USTA Teams: </h4>";
+    
+        $retv.=  "<h4>Friday night clinics: </h4>";
+        $retv.=  "<h4>Saturday Mixers: including the free Pig-out</h4>";
+        $retv.=  "<h4>The club singles ladder for men and women </h4>";
+        $retv.=  "<h4>The end of year dinner</h4>";
+        $retv.=  "<h4></h4>";
+        $retv.=  "<h4>Santa Clara Tennis Club</h4>";
+        $retv.=  "</tr>";
+        $retv.=  "</table>";
+    
+        $retv.=  "<p>";
+        $retv.=  "<p>";
+    
+        $retv.=  "</body>";
+        $retv.=  "</html>";
+    
+        return $retv;
+    
+    }
 ?>
