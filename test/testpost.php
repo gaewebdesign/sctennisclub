59a2248bf4271d85e1dde7657d399f70e91536bf
githubmutt@gmail.com <github.mutt@gmail.com>
2025-03-03 18:53:50 -0800

    testpost

---
+++ test/testpost.php
@@ -0,0 +1,23 @@
+<?php
+
+      include "../library/include.inc";
+
+//      $CUSTOM = $_POST["custom"];
+
+      echo("join_/notify.php: enumerate _POST array <br>");
+      foreach ($_POST as $key => $value) {
+         LOGGER("testpost.php POST: $key -> $value ");
+         echo("testpost.php POST: $key -> $value \n<br>");
+      }
+      echo("<p");
+
+      echo("join_/notify.php: enumerate _GET array <br>");
+      foreach ($_GET as $key => $value) {
+        LOGGER("testpost.php GET $key -> $value ");
+         echo("testpost.php GET: $key -> $value \n<br>");
+      
+      }
+      
+
+?>
+
