5c2604c4d12e728a4ba5965c0cac6643c6b52ef0
githubmutt@gmail.com <github.mutt@gmail.com>
2025-03-12 17:09:12 -0700

    croncurl2

---
+++ join_/croncurl2.php
@@ -0,0 +1,25 @@
+<?php
+include "../library/include.inc";
+include "../library/emailer.php";
+include "../library/email/email.inc";
+
+    LOGS("croncurl2.php  calling croncapt.php ");
+    $url = "http://www.sctennisclub.org/join_/croncapt.php";
+    $message = "croncurl2.php calling $url";
+    phpemailer($subject,$message , $recipient , "south@sctennisclub.org");
+
+/*
+CRON JOB 
+php -q /home/southb56/sctennisclub.org/join_/croncurl2.php
+*/
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$response = curl_exec( $ch);
+
+echo $response;
+
+
+?>
