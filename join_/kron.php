c13b067c5e784c0ed2f942c30e56dd3f88c07a66
githubmutt@gmail.com <github.mutt@gmail.com>
2025-03-16 01:31:45 -0700

    kron

--- join_/kron.php
+++ join_/kron.php
@@ -3,13 +3,13 @@ include "../library/include.inc";
 include "../library/emailer.php";
 include "../library/email/email.inc";
 
-    LOGS("croncurl2.php  calling croncapt.php ");
+    LOGS("kron.php  calling croncapt.php ");
     $url = "http://www.sctennisclub.org/join_/croncapt.php";
-    $subject = "croncurl2.php calling $url";
-    $message = "croncurl2.php calling $url";
+    $subject = "kron.php calling $url";
+    $message = "kron.php calling $url";
     $recipient = "south@sctennisclub.org";
 
-    
+
     phpemailer($subject,$message , $recipient , "south@sctennisclub.org");
 
 /*
@@ -23,7 +23,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $response = curl_exec( $ch);
 
-echo $response;
+//echo $response;
 
 
 ?>
