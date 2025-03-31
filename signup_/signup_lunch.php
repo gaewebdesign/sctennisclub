2bef99f1d0c9281afa6d2c97785144d5f2fbdc69
githubmutt@gmail.com <github.mutt@gmail.com>
2025-03-31 01:00:55 -0700

    signup

--- signup_/signup_lunch.php
+++ signup_/signup_lunch.php
@@ -287,7 +287,7 @@
 <button id="LunchButton" class="w-100 btn btn-primary btn-lg" disabled > Event done </button>
 -->
 <div class="container" id="LunchDone" >
-<button id="LunchButto" class="w-100 btn btn-primary btn-lg"   > Upcoming Event </button>
+<button id="LunchButto" class="w-100 btn btn-primary btn-lg" disabled> Upcoming Event </button>
 </div>
 
         </form>
