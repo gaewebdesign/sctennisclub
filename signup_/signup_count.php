13ab3aef74e96925b3e7af51571df617eeb7d939
githubmutt@gmail.com <github.mutt@gmail.com>
2025-04-05 19:07:28 -0700

    auto shutoff

--- signup_/signup_count.php
+++ signup_/signup_count.php
@@ -3,18 +3,19 @@
 include "../library/include.inc";
 
 //$con = DBMembership();
-$con = DBMembership();
+
 $d = 0;
 
 
 $cretaceous = strtotime('2025-5-5');
           
 $epoch = strtotime('2025-3-30');
-// Switch between tables here **************************
+
 
 $query = "select * from ".TABLE_MIXER_PAYPAL."  where custom>$epoch and custom<$cretaceous order by fname asc";
 
-$con = Configure();
+//$con = Configure();
+$con = DBMembership();
 
 $qr=mysqli_query($con,$query);
 $r = mysqli_num_rows($qr);
