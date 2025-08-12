3a00575c5474e2c43071873706dd609800b88ded
githubmutt@gmail.com <github.mutt@gmail.com>
2025-08-11 23:55:11 -0700

    2025 ice cream

--- signup_/signup_icecream.php
+++ signup_/signup_icecream.php
@@ -21,7 +21,7 @@
     <h3 style="font-size: 1.2em ">
     Enjoy some  tennis and <b>ice cream</b>  on the courts with fellow members for
     social mixed doubles round robin play at Santa Clara Tennis Center on
-    August 22, 2025 , 9-12 noon on courts #6-#8.
+    August 23, 2025 , 9-12 noon on courts #6-#8.
     If you just &#x1F497; ice cream, you may
     sign up for the ice cream only option! <br/>
     Signup page is at <a href="http://www.sctennisclub.org/icecream">www.sctennisclub.org/icecream</a>
@@ -90,7 +90,7 @@
 
         <div class="container-fluid mt-2">
       <div class="mb-5 col-12">
-      <button class="btn btn-primary" disabled  name="SubmitButton" type="submit">Go to Paypal</button>
+      <button class="btn btn-primary"   name="SubmitButton" type="submit">Go to Paypal</button>
       </div  >
       </div>
 
