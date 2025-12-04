c27a44b699a194d9083a54599271bcf8934d321b
githubmutt@gmail.com <github.mutt@gmail.com>
2025-12-03 16:09:59 -0800

    email

--- members_/email.php
+++ members_/email.php
@@ -96,7 +96,7 @@ Please Note: Starting May 1st, our phone availability will change from 24/7 to M
 
 //        $query = "select * from ".TABLE_PAYPAL." where ( year BETWEEN $_YEAR and $YEAR ) order by lname ";
         DEBUG( $query);
-        echo ($_YEAR);
+      //  echo ($_YEAR);
         
          $qr=mysqli_query($con,$query);
                   while ($row = mysqli_fetch_assoc($qr)) {  
